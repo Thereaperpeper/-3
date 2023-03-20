1f5451e7a63161c5fd0b6f33e6ecfdee370181c0
Thereaperpeper <71146725+Thereaperpeper@users.noreply.github.com>
2023-03-20 22:40:55 +0100

    Update ee.lua

--- ee.lua
+++ ee.lua
@@ -1,12 +1,10 @@
 local whitelisted = {"E8BFD673-5CE5-4125-AE25-32C2CD939E34", "E8BFD673-5CE5-4125-AE25-32C2CD939E34"}
 
--- Rick, Curved
-
 
 local e = false
 -- first check if user is whitelisted
 for x, c in pairs (whitelisted) do
-    if c == game:GetService("RbxAnalyticsService"):GetClientId() then
+    if c == tostring(game:GetService("RbxAnalyticsService"):GetClientId()) then
         e = true
     end
 end
