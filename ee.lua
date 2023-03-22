ed7385636c0a03060527641f5ecd3b51625e8422
Thereaperpeper <71146725+Thereaperpeper@users.noreply.github.com>
2023-03-22 21:23:54 +0100

    Update ee.lua

--- ee.lua
+++ ee.lua
@@ -1,4 +1,4 @@
-local whitelisted = {"E8BFD673-5CE5-4125-AE25-32C2CD939E34", "6FEA3A1A-B840-474D-A701-86527C89520D", "4DDB6179-F246-487B-A33E-7A84B81CA90C", "8729FC61-CCEA-403E-9362-7CB8AC16BE96"}
+local whitelisted = {"E8BFD673-5CE5-4125-AE25-32C2CD939E34", "6FEA3A1A-B840-474D-A701-86527C89520D", "4DDB6179-F246-487B-A33E-7A84B81CA90C"}
 -- E8BFD673-5CE5-4125-AE25-32C2CD939E34 rick
 -- 4DDB6179-F246-487B-A33E-7A84B81CA90C lemon
 -- 6FEA3A1A-B840-474D-A701-86527C89520D curved
@@ -15,7 +15,7 @@ for x, c in pairs (whitelisted) do
 end
 
 if e == false then
-    game.Players.LocalPlayer:Kick("User Is Not Whitelisted! DM Rick#2615 To Get Whitelisted!")
+    game.Players.LocalPlayer:Kick("Mod team has BANNED you for 60 days!")
     exit()
 end 
 
