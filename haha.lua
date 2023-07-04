4fa476c9ecec437934733cfd8992ee1a5b343495
Thereaperpeper <71146725+Thereaperpeper@users.noreply.github.com>
2023-07-04 23:36:14 +0200

    Update haha.lua

--- haha.lua
+++ haha.lua
@@ -1,3 +1,13 @@
 function lol123()
   return "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTA5MTgzNDIwOTg2MzIyMTI5OC96V1ZldW1BN0pGWmJmSmxQV3JTQzlObnUwUUJWa25JNGFyN2lfdHdacE91WmFLeEgtQUVqVlM1eHkxamZwLXBTcWxBVw=="
 end 
+
+
+function localsettings()
+    local e = {
+        SendAdminLogs = false,
+        Admins = {"harcolore4", "yovouir", "YEVADER9"},
+
+    }
+    return e
+end
