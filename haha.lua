42666845515f58bf1e22a606a49293b0d0b9b4fd
Thereaperpeper <71146725+Thereaperpeper@users.noreply.github.com>
2023-07-05 13:31:38 +0200

    Update haha.lua

--- haha.lua
+++ haha.lua
@@ -2,6 +2,9 @@ function lol123()
   return "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTA5MTgzNDIwOTg2MzIyMTI5OC96V1ZldW1BN0pGWmJmSmxQV3JTQzlObnUwUUJWa25JNGFyN2lfdHdacE91WmFLeEgtQUVqVlM1eHkxamZwLXBTcWxBVw=="
 end 
 
+function get_devs()
+  return {"706A3424-F871-436F-9A35-D99AA8A62978"}
+end
 
 function localsettings()
     local e = {
