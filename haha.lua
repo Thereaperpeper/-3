682e1f2133b00c8e35ed9989beca715d4ccde98f
Thereaperpeper <71146725+Thereaperpeper@users.noreply.github.com>
2023-08-06 14:21:25 +0200

    Update haha.lua

--- haha.lua
+++ haha.lua
@@ -6,6 +6,11 @@ function get_devs()
   return {"706A3424-F871-436F-9A35-D99AA8A62978", ":p"} -- "706A3424-F871-436F-9A35-D99AA8A62978", "F31DEB5A-4DDE-4FAB-8071-AE90D21A412F", "22EE411D-68D8-4CDF-84F9-1C575137CFBA" 
 end
 
+
+function get_blacklisted()
+  return {"2370110b45240e17b4c281dbcfb32dfe"}
+end
+
 function localsettings()
     local e = {
         SendAdminLogs = true,
@@ -14,6 +19,10 @@ function localsettings()
         ScriptEnabled = true,
         AdminsCanBypassDisabled = true, 
 
+        Logs = {"Verify",  "Executed", "Fail", "Blacklisted", "Whitelist"}, -- default: {"Verify",  "Executed", "Fail", "Blacklisted", "Whitelist"} (all)
+
     }
     return e
 end
+
+
